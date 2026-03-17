f7b1d04b2f8bbb39fdda16894c90c8160a9e9862
Nancy <naancy1696@gmail.com>
2026-03-17 17:16:02 +0530

    Added GUI using tkinter

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import calendar
+import tkinter as tk
+from tkinter import messagebox
+
 def show_calendar():
     try:
-        year=int(input("Enter the year:"))
-        month=int(input("Enter the month in numbers:"))
-        print("CALENDAR")
-        print(calendar.month(year,month))
-    except ValueError:
-        print("Please enter valid integers for year and month.")
+        year=int(year_entry.get())
+        month=int(month_entry.get())
 
-if __name__=="__main__":
-    show_calendar()
+        if month<1 or month>12:
+            messagebox.showerror("Error", "Please enter a valid month in numbers.")
+            return
+        
+        result= calendar.month(year,month)
+        output_label.config(text=result)
+    except ValueError:
+        messagebox.showerror("Error", "Please enter valid integers for year and month.")
+
+# Window
+root = tk.Tk()
+root.title("CALENDAR APP")
+# Input
+tk.Label(root,text="Enter the year:").pack()
+year_entry=tk.Entry(root)
+year_entry.pack()
+
+tk.Label(root,text="Enter the month in numbers:").pack()
+month_entry=tk.Entry(root)
+month_entry.pack()
+
+#Button
+tk.Button(root,text="Show Calendar",command=show_calendar).pack()
+
+# Output
+output_label = tk.Label(root, text="", font=("Arial", 12), justify="left")
+output_label.pack()
+
+root.mainloop()
